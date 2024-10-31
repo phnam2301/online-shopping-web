11153072c950e10a67288e686f9bfa805358957d
Phan Hoang Nam <phnam230197@gmail.com>
2024-10-31 22:10:54 +0700

    add files

---
+++ src/test/java/com/fkn/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fkn.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
