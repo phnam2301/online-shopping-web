3d38b50846157db328278789194931760e4d67a3
William Phan <phnam230197@gmail.com>
2025-08-08 04:39:28 +0700

    add front-end folder\nuser Thymeleaf dependency

---
+++ front-end/src/main/java/vn/chuot96/shop/ShopApplication.java
@@ -0,0 +1,13 @@
+package vn.chuot96.shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopApplication.class, args);
+	}
+
+}
---
+++ front-end/src/test/java/vn/chuot96/shop/ShopApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.chuot96.shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
